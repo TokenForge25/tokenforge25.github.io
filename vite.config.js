88b2d285078752f6377628a4f475dafc273aacf0
aiadmin <vmn2014@gmail.com>
2025-05-20 12:57:24 +0300

    Fix Solana Web3.js dependencies and build config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import viteCompression from 'vite-plugin-compression'
+import inject from '@rollup/plugin-inject'
 
 export default defineConfig({
   base: '/',
@@ -8,12 +9,17 @@ export default defineConfig({
     react(),
     viteCompression({ algorithm: 'brotliCompress', threshold: 10240 })
   ],
+  define: {
+    global: {},
+    'process.env.NODE_DEBUG': JSON.stringify('')
+  },
   build: {
     outDir: 'build',
     sourcemap: false,
     minify: 'terser',
     cssMinify: true,
     rollupOptions: {
+      plugins: [inject({ Buffer: ['buffer', 'Buffer'] })],
       output: {
         manualChunks: {
           solana: ['@solana/web3.js', '@solana/spl-token'],
@@ -21,5 +27,13 @@ export default defineConfig({
         }
       }
     }
+  },
+  optimizeDeps: {
+    include: [
+      '@solana/web3.js',
+      '@solana/spl-token',
+      'buffer',
+      'process'
+    ]
   }
 })
