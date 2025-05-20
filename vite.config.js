b4c9da7e9b6a89cfcdb3a25d4c902cb3005dac81
aiadmin <vmn2014@gmail.com>
2025-05-20 09:14:30 +0300

    Remove critters plugin temporarily to resolve dev server issue

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Critters from 'critters';
 import viteCompression from 'vite-plugin-compression';
 
 export default defineConfig({
   base: '/',
   plugins: [
     react(),
-    {
-      name: 'critters',
-      apply: 'build',
-      enforce: 'post',
-      transformIndexHtml: (html) => {
-        const critters = new Critters({
-          preload: 'swap',
-          pruneSource: true,
-          reduceInlineStyles: false,
-        });
-        return critters.process(html);
-      },
-    },
     viteCompression({ algorithm: 'brotliCompress', threshold: 10240 }),
   ],
   build: {
